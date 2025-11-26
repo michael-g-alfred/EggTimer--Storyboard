ff1eec80d52e502a79b4d91b2f0703f0267491f7
michael-g-alfred <Michoolgeorge@gmail.com>
2025-11-26 23:59:47 +0200

    Initial Commit

---
+++ EggTimer--Storyboard/ViewController.swift
@@ -0,0 +1,79 @@
+    //
+    //  ViewController.swift
+    //  EggTimer--Storyboard
+    //
+    //  Created by Michael George on 26/11/2025.
+    //
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    let eggTime: [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var secondPassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    var player: AVAudioPlayer?
+    
+    
+    @IBAction func hardnessSelectedBtn(_ sender: UIButton) {
+        timer.invalidate()
+        
+        guard let hardness = sender.currentTitle else { return }
+        guard let time = eggTime[hardness] else { return }
+        
+        titleLabel.text = "Cracking Egg..."
+        
+        totalTime = time
+        secondPassed = 0        // أهمّ سطر كان ناقص
+        
+        timerProgress.progress = 0
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Error loading sound file")
+        }
+    }
+    
+    
+    @objc func updateTimer() {
+        if secondPassed < totalTime {
+            secondPassed += 1
+            
+            let progress = Float(secondPassed) / Float(totalTime)
+            timerProgress.progress = progress
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "BOOM!"
+            timerProgress.progress = 1
+            playSound()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timerProgress.progress = 0
+    }
+}
